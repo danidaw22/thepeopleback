34af45d48cbcfba740d9329fa4ae156d0ac58010
danidaw22 <danidaw2019@gmail.com>
2021-04-12 12:48:46 +0200

    Cambio controlador persona

--- src/controllers/persona.js
+++ src/controllers/persona.js
@@ -1,17 +1,17 @@
-const peopleController = {}
+const personaController = {}
 
-const People = require('../models/persona.js')
-
-
-peopleController.detail = async(req, res) => {}
-
-peopleController.update = async(req, res) => {}
-
-peopleController.delete = async(req, res) => {}
-
-peopleController.create = async(req, res) => {}
+const Persona = require('../models/persona.js')
 
 
+personaController.detail = async(req, res) => {}
+
+personaController.update = async(req, res) => {}
+
+personaController.delete = async(req, res) => {}
+
+personaController.create = async(req, res) => {}
 
 
-module.exports = peopleController
+
+
+module.exports = personaController
